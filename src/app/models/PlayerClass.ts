33b10618c3ff2bdf3b221c942056b1c053ba1ac6
Patrice <info@patrice.codes>
2020-02-26 20:15:38 +0100

    Initial commit

---
+++ src/app/models/PlayerClass.ts
@@ -0,0 +1,11 @@
+enum PlayerClass {
+    Rogue = 1,
+    Shaman = 2,
+    Warlock = 3,
+    Warrior = 4,
+    Druid = 5,
+    Mage = 6,
+    Hunter = 7,
+    Priest = 8,
+    Paladin = 9,
+};
---
+++ src/app/models/Spec.ts
@@ -0,0 +1,5 @@
+export enum Specc {
+    DD ,
+    Heal ,
+    Tank ,
+}
